5879216760e651d97e7754801364019af145a3c0
AnyelyGomez <anyelitagn@gmail.com>
2019-10-23 14:01:13 -0500

    D1

--- dshopping/administration/admin.py
+++ dshopping/administration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+admin.site.register(Category)
+admin.site.register(Country)
+admin.site.register(Clients)
+admin.site.register(Gender)
+admin.site.register(Product)
+admin.site.register(Shopping)
--- dshopping/dshopping/settings.py
+++ dshopping/dshopping/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'administration'
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ DATABASES = {
     'default': {
         
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'messenger',
+        'NAME': 'dshopping',
         'USER': 'postgres',
         'PASSWORD': 'unicesmag',
         'HOST': 'localhost',
